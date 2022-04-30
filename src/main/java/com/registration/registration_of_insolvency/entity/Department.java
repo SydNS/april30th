86e67292522a953621bbddecc0b80396738aeac4
SydNS <sydneynsiimbe@gmail.com>
2022-04-30 22:18:48 +0300

    validating fields

--- src/main/java/com/registration/registration_of_insolvency/entity/Department.java
+++ src/main/java/com/registration/registration_of_insolvency/entity/Department.java
@@ -1,21 +1,37 @@
 package com.registration.registration_of_insolvency.entity;
 
+import com.registration.registration_of_insolvency.contoller.response.DeptResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Data
+@Schema
+@NoArgsConstructor
 public class Department {
     @Id
-    @Column(name = "id", nullable = false)
+    @Column
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     @Column
-    private String DeptName;
+    @NotEmpty
+    private String deptName;
     @Column
-    private String DeptAddress;
+    @NotEmpty
+    private String deptAddress;
     @Column
-    private String DeptCode;
+    @NotEmpty
+    private String deptCode;
+
+    public Department(String deptName, String deptAddress, String deptCode) {
+        this.deptName = deptName;
+        this.deptAddress = deptAddress;
+        this.deptCode = deptCode;
+    }
+    public DeptResponse asResponse(){ return new DeptResponse(deptName,deptAddress,deptCode);}
 }
