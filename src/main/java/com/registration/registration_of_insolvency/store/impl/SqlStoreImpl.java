d35749a01bb021c0e408b98901c761b20b9518c0
SydNS <sydneynsiimbe@gmail.com>
2022-04-30 22:20:59 +0300

    added repository use

--- src/main/java/com/registration/registration_of_insolvency/store/impl/SqlStoreImpl.java
+++ src/main/java/com/registration/registration_of_insolvency/store/impl/SqlStoreImpl.java
@@ -1,25 +1,30 @@
 package com.registration.registration_of_insolvency.store.impl;
 
 import com.registration.registration_of_insolvency.entity.Department;
+import com.registration.registration_of_insolvency.repository.DepartmentRepository;
 import com.registration.registration_of_insolvency.store.DepartmentStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 @Component
 public class SqlStoreImpl implements DepartmentStore {
+    @Autowired
+    private DepartmentRepository departmentRepository;
     @Override
     public Department createDepartment(Department department) {
-        return null;
+        return departmentRepository.save(department);
     }
 
     @Override
     public Optional<Department> findDepartmentById(Integer id) {
-        return Optional.empty();
+        Optional<Department> department=departmentRepository.findById(id);
+        return department;
     }
 
     @Override
     public List<Department> allDepartments() {
-        return null;
+        return departmentRepository.findAll();
     }
 }
