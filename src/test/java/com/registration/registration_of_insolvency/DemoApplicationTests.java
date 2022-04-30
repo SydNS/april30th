4d62c53fe0989602b024733c8a3922d2f09bd12f
SydNS <sydneynsiimbe@gmail.com>
2022-04-30 20:29:38 +0300

    added the h2 db to use

---
+++ src/test/java/com/registration/registration_of_insolvency/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registration.registration_of_insolvency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
