dbc47fc7f1e2bfcb8318ad8a5f899a7733ec129d
SydNS <sydneynsiimbe@gmail.com>
2022-04-30 22:19:06 +0300

    using hte sql db store

--- src/main/java/com/registration/registration_of_insolvency/service/DepartmentService.java
+++ src/main/java/com/registration/registration_of_insolvency/service/DepartmentService.java
@@ -3,6 +3,7 @@ package com.registration.registration_of_insolvency.service;
 import com.registration.registration_of_insolvency.entity.Department;
 import com.registration.registration_of_insolvency.store.DepartmentStore;
 import com.registration.registration_of_insolvency.store.impl.InMemStoreImpl;
+import com.registration.registration_of_insolvency.store.impl.SqlStoreImpl;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,14 +14,14 @@ import java.util.Optional;
 @Service
 public class DepartmentService {
     @Autowired
-    private InMemStoreImpl departmentStore;
+    private SqlStoreImpl departmentStore;
 
     public Department createDepartment(Department department) {
         return departmentStore.createDepartment(department);
     }
 
-    public Optional<Department> findDepartmentById(@NonNull Integer id) {
-        return departmentStore.findDepartmentById(id);
+    public Department findDepartmentById(Integer id) {
+        return departmentStore.findDepartmentById(id).get();
     }
     public List<Department> allDepartments() {
         return departmentStore.allDepartments();
