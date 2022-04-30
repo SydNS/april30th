c7d06b2894913f182132c70f91425cc3df605ff2
SydNS <sydneynsiimbe@gmail.com>
2022-04-30 22:22:00 +0300

    cleans the main file

--- src/main/java/com/registration/registration_of_insolvency/DemoApplication.java
+++ src/main/java/com/registration/registration_of_insolvency/DemoApplication.java
@@ -2,10 +2,8 @@ package com.registration.registration_of_insolvency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class DemoApplication {
 
 	public static void main(String[] args) {
